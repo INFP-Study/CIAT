72adf29751eef6ba7ff793a5b453211c09e30d52
thyoondev <th.yoon.dev@gmail.com>
2021-09-13 21:00:06 +0900

    [PROJECT] E2-S3 프론트엔드 세팅 #7

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TitleTest = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: red;
+`;
 
 function index() {
-  return <div>home</div>;
+  return (
+    <div>
+      <TitleTest>test</TitleTest>
+      <br />
+      home
+    </div>
+  );
 }
 
 export default index;
