37659d1e860e5fa17634fb3fc792ee331b218c71
hskoo <nrudev812@gmail.com>
2021-09-13 20:48:21 +0900

    chore : [PROJECT] E2-S6 백엔드 세팅
    - 백엔드 스프링 부트 프로젝트 생성

---
+++ backend/src/test/java/com/infp/ciat/CiatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infp.ciat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
