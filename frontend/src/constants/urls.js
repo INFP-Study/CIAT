f9f77d91c755f62eca5a3ed64f78e4945564c92e
thyoondev <th.yoon.dev@gmail.com>
2021-10-06 20:46:38 +0900

    [PROJECT] E2-S5 유틸함수 작성 #11 상수 파일 추가하기

---
+++ frontend/src/constants/urls.js
@@ -0,0 +1,9 @@
+export const MAIN_URL = '/';
+export const ADMIN_PAGE_URL = '/admin';
+export const MY_PAGE_URL = '/mypage';
+export const FEED_URL = '/feed';
+export const PLANT_MANAGEMENT_URL = '/plantmanagement';
+
+export const SIGNIN_URL = '/signin';
+export const SIGNUP_URL = '/signup';
+export const AUTH_URL = '/auth';
