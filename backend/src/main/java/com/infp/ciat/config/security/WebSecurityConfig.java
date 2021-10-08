5c3200b59ff269f4e0c1e67db15fb5d8fcb34b8d
choisungwook <kgg1959@naver.com>
2021-10-08 19:22:04 +0900

    [FEATURE] E4-S1 로그인 API - security설정 위치 이동 #28

--- backend/src/main/java/com/infp/ciat/config/security/WebSecurityConfig.java
+++ backend/src/main/java/com/infp/ciat/config/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.infp.ciat.config;
+package com.infp.ciat.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
