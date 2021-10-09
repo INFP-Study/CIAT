6654ff0adbe04bfc66a4ec8106b5f205386f12d0
choisungwook <kgg1959@naver.com>
2021-10-09 18:19:16 +0900

    [FEATURE] E4-S1 로그인 API - 로그인 성공시 jwt토큰발급  #28

--- backend/src/test/java/com/infp/ciat/config/security/LoginfilterTest.java
+++ backend/src/test/java/com/infp/ciat/config/security/LoginfilterTest.java
@@ -2,6 +2,8 @@ package com.infp.ciat.config.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.infp.ciat.user.controller.dto.request.SignupRequestDTO;
+import com.infp.ciat.user.repository.AccountRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -21,6 +25,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class LoginfilterTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @AfterEach
+    public void AfterEach() {
+        accountRepository.deleteAllInBatch();
+    }
 
     @Test
     @DisplayName("회원가입 후 로그인")
@@ -44,6 +56,34 @@ class LoginfilterTest {
                 )
                 .andExpect(status().isOk())
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원가입 후 로그인, jwt토큰 확인")
+    public void SignUpAndPrintToken() throws Exception {
+        SignupRequestDTO signup_requestdto1 = create_signup_requestdto("test1@test.com", "test1", "password");
+
+        mockMvc.perform(
+                post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(signup_requestdto1))
+        )
+                .andExpect(status().isCreated())
+                .andDo(print());
+
+        RequestLoginDTO requestLoginDTO1 = new RequestLoginDTO(signup_requestdto1.getEmail(), signup_requestdto1.getPassword());
+
+        MvcResult signin_result = mockMvc.perform(
+                post("/signin")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(new ObjectMapper().writeValueAsString(requestLoginDTO1))
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String authorization = signin_result.getResponse().getHeader("Authorization");
+        assertNotNull(authorization);
+        System.out.println("jwt token: " + authorization);
     }
 
     /***
