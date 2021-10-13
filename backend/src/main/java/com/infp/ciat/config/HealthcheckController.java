81bbd4562fe2fd02d050c3015ebbc21b17e1e19b
choisungwook <kgg1959@naver.com>
2021-10-13 23:11:33 +0900

    [PROJECT] E2-S7 AWS 자동 배포 설정 - backend-dev probe시간 추가 #16

--- backend/src/main/java/com/infp/ciat/config/HealthcheckController.java
+++ backend/src/main/java/com/infp/ciat/config/HealthcheckController.java
@@ -1,6 +1,7 @@
 package com.infp.ciat.config;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /***
  * 쿠버네티스 healthcheck 컨트롤러
