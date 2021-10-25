c4828f96e7cd6304ae32fa96d7a0840edfca9101
chlwlgus4 <chlwlgus89@naver.com>
2021-10-25 21:38:31 +0900

    [FEATURE] E7-S2 산림청 api #53

--- backend/src/main/java/com/infp/ciat/common/properties/KeyProperties.java
+++ backend/src/main/java/com/infp/ciat/common/properties/KeyProperties.java
@@ -9,7 +9,8 @@ import org.springframework.context.annotation.PropertySource;
 @Data
 @Configuration
 @EnableConfigurationProperties
-@PropertySource("classpath:properties/key-${spring.profiles.active}.properties")
+//@PropertySource("classpath:properties/key-${spring.profiles.active}.properties")
+@PropertySource("classpath:properties/key-local.properties")
 public class KeyProperties {
 
     @Value("${forest.serviceKey}")
