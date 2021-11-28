5a40bbeecfdd47f10fcdb71cde7c380e9a2ef461
choisungwook <kgg1959@naver.com>
2021-11-28 17:43:33 +0900

    E5-S7 이미지 업로드 #124 - Return type변경

--- backend/src/main/java/com/infp/ciat/board/service/UploadImagesService.java
+++ backend/src/main/java/com/infp/ciat/board/service/UploadImagesService.java
@@ -3,6 +3,7 @@ package com.infp.ciat.board.service;
 import com.amazonaws.SdkClientException;
 import com.infp.ciat.board.dto.CreateBoardRequestForm;
 import com.infp.ciat.common.aws.S3Helper;
+import com.infp.ciat.common.exceptions.FailCreateBoard;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,8 +30,8 @@ public class UploadImagesService {
     /***
      * 이미지 업로드
      */
-    public String Upload(CreateBoardRequestForm requestForm){
-        String uploaded_s3url = "";
+    public List<String> Upload(CreateBoardRequestForm requestForm) throws FailCreateBoard {
+        List<String> results = new ArrayList<>();
 
         log.info("--- file upload start ---");
         List<MultipartFile> files = requestForm.getMultipartHttpServletRequest().getFiles("file");
@@ -37,15 +39,18 @@ public class UploadImagesService {
             for(MultipartFile file : files){
                 log.info(String.format("to upload file: %s", file.getOriginalFilename()));
                 try{
-                    uploaded_s3url = s3Helper.uploadFile(bucket_name, file);
+                    String uploaded_s3url = s3Helper.uploadFile(bucket_name, file);
                     log.debug(String.format("%s file upload is success. url is: %s", bucket_name, uploaded_s3url));
+
+                    results.add(uploaded_s3url);
                 }catch(SdkClientException | IOException e){
                     log.error(String.format("File Upload failed and exception: %s", e.toString()));
+                    throw new FailCreateBoard("이미지 업로드 실패");
                 }
             }
         }
         log.info("--- file upload done ---");
-        return uploaded_s3url;
+        return results;
     }
 
     /***
--- backend/src/main/java/com/infp/ciat/common/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/infp/ciat/common/exceptions/GlobalExceptionHandler.java
@@ -10,13 +10,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     /***
-     * 회원가입 시 중복오류 핸들러
+     * 회원가입 시 중복 예외핸들러
      * @param ex
      * @return
      */
     @ExceptionHandler(UserExistException.class)
     public ResponseEntity<ErrorResponse> UserExistException(UserExistException ex){
         ErrorResponse response = new ErrorResponse(HttpStatus.CONFLICT, "회원가입 실패", ex.getMessage());
+        return new ResponseEntity<>(response, response.getHttp_status());
+    }
+
+    /***
+     * 게시판 생성실패 예외핸들러
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(FailCreateBoard.class)
+    public ResponseEntity<ErrorResponse> FailCreateException(FailCreateBoard ex){
+        ErrorResponse response = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "게시판 생성실패", ex.getMessage());
         return new ResponseEntity<>(response, response.getHttp_status());
     }
 }
