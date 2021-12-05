a682db82669fd6a5afa441351594c7c0496a9971
choisungwook <kgg1959@naver.com>
2021-12-05 16:57:55 +0900

    Revert "refactor: Board 이름을 Feed로 모두 변경"

--- backend/src/main/java/com/infp/ciat/board/dto/CreateBoardRequestForm.java
+++ backend/src/main/java/com/infp/ciat/board/dto/CreateBoardRequestForm.java
@@ -1,8 +1,10 @@
-package com.infp.ciat.feed.dto;
+package com.infp.ciat.board.dto;
 
 import lombok.*;
-
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.util.List;
 
 /***
  * 게시판(피드)생성 요청 dto
@@ -11,13 +13,13 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 @Getter
 @NoArgsConstructor
 @ToString
-public class CreateFeedRequestForm {
+public class CreateBoardRequestForm {
     private String title;
     private String content;
     private MultipartHttpServletRequest multipartHttpServletRequest;
 
     @Builder
-    public CreateFeedRequestForm(String title, String content, MultipartHttpServletRequest multipartHttpServletRequest) {
+    public CreateBoardRequestForm(String title, String content, MultipartHttpServletRequest multipartHttpServletRequest) {
         this.title = title;
         this.content = content;
         this.multipartHttpServletRequest = multipartHttpServletRequest;
