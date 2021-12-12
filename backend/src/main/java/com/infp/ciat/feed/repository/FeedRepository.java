f8d4bbab98e74db94ee98a30989d37c6c09a8742
hskoo <nrudev812@gmail.com>
2021-12-12 16:51:44 +0900

    refactor : FeedRepository 쿼리문 수정

--- backend/src/main/java/com/infp/ciat/feed/repository/FeedRepository.java
+++ backend/src/main/java/com/infp/ciat/feed/repository/FeedRepository.java
@@ -1,8 +1,11 @@
 package com.infp.ciat.feed.repository;
 
 import com.infp.ciat.feed.entity.Feed;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -10,10 +13,10 @@ import java.util.Optional;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
 
-    @Query(nativeQuery = true, value = "SELECT * FROM menu WHERE show_yn = 'Y'")
+    @Query(nativeQuery = true, value = "SELECT * FROM feed WHERE show_yn = 'Y'")
     List<Feed> findAllNotDeleted();
 
-    @Query(nativeQuery = true, value = "SELECT * FROM menu WHERE id = :id AND show_yn = 'Y'")
+    @Query(nativeQuery = true, value = "SELECT * FROM feed WHERE id = :id AND show_yn = 'Y'")
     Optional<Feed> findByIdNotDeleted(@Param("id") Long id);
 
 }
