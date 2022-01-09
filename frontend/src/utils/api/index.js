ee1fdc5751b58aad5556f1e370e4c98912ce1ff5
thyoondev <thyoondev@gmail.com>
2022-01-09 16:56:07 +0900

    chore : change endpoint url

--- frontend/src/utils/api/index.js
+++ frontend/src/utils/api/index.js
@@ -3,7 +3,7 @@ const isDev = window.location.origin.includes('localhost');
 //   ? 'http://localhost:3000'
 //   : 'https://ciat-bakend.choicloudlab.com';
 
-const SERVER_ORIGIN = 'https://ciat-bakend.choicloudlab.com';
+const SERVER_ORIGIN = 'https://ciat-dev.choicloudlab.com';
 
 export const API_DOMAIN = SERVER_ORIGIN;
 export const API_END_POINT = `${SERVER_ORIGIN}/api/v1`;
